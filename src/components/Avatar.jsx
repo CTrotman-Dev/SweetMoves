73482fa0640a1b53ca62495b7159487245ca749a
CTrotman-Dev <c.trotman92@gmail.com>
2023-07-30 17:47:08 +0100

    Updates

--- src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Avatar(props) {
-    return(
-        <img className="circle-img" src={props.img} alt="Avatar image" />
-    );
-}
-
-export default Avatar;
--- src/components/CardDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function CardDetails(props) {
-    return (
-        <p className="info">{props.detailInfo}</p>
-    );
-}
-
-export default CardDetails;
