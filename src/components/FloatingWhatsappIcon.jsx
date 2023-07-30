58a725b0f0bbfadcb8a804dcc60fe62409be30f8
CTrotman-Dev <c.trotman92@gmail.com>
2023-07-30 18:36:15 +0100

    links v2

--- src/components/FloatingWhatsappIcon.jsx
+++ src/components/FloatingWhatsappIcon.jsx
@@ -11,7 +11,7 @@ const FloatingWhatsappIcon = () => {
 
     return (
         <div className="floating-icons">
-            <a href="https://api.whatsapp.com/send?phone=07874303736">
+            <a href="https://api.whatsapp.com/send?phone=+447874303736">
                 <FontAwesomeIcon className="social-icon" size="6x" beat icon={faWhatsapp} />
             </a>
 
