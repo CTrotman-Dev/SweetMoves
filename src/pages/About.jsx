e345a938fb501503b0b513ebe6e662c0d3772027
CTrotman-Dev <c.trotman92@gmail.com>
2023-08-07 21:00:48 +0100

    update about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,23 +11,28 @@ const About = () => {
             <TitleBanner text="About" />
             <div className="col-md-6">
             <div className="about-content">
-                <h1>
-                    About Us
-                </h1>
-                <p> SweetMoves is a family-run home removals company that operates out of Cardiff. We offer a wide range of services, including house moves, office moves, and storage.
-                </p>
-                <p>
-                    We believe that moving should be a positive experience, and we're committed to making sure that our customers are happy with our services. We offer 24/7 service, so we can accommodate your schedule. And we offer an additional fee for moves that take place between 21:00 and 06:00.
-                </p>
-                <p>
-                    We're confident that we can provide you with the best possible moving experience. So please contact us today to schedule a free consultation.
-                </p>
-                {/* <p>
-                SweetMoves was founded in 2001 by [owner's name] and his wife. They started the company because they wanted to help people move their belongings with ease and peace of mind.
-                </p>
-                <p>
-                Over the past 20 years, SweetMoves has grown to become one of the leading home removals companies in the area. We have a team of experienced and professional movers who are dedicated to providing our customers with the best possible service.
-                </p> */}
+            <h1>SweetMoves: Making your move a sweet success</h1>
+                    {/* <h1>SweetMoves: Moving made sweet and easy, with a smile.</h1> */}
+                    <p>
+                        Welcome to SweetMoves, the Cardiff-based company that operates across the UK, offering home and office removals, storage, and packaging services.
+                    </p>
+                    <p>
+                        Whether you are moving across the street or across the country, we are here to make your move as sweet as possible. We have over 3 years of experience in the moving industry, and we pride ourselves on our professionalism, reliability, and affordability.
+                    </p>
+                    <p>
+                        We have a fleet of modern vehicles, a team of trained and friendly staff, and a range of flexible options to suit your needs and budget. We also provide free quotes, insurance cover, and customer support throughout your move. At SweetMoves, we understand that moving can be stressful and time-consuming, but we are here to take care of everything for you.
+                    </p>
+                    <p>
+                        From packing your belongings with care and efficiency, to transporting them safely and securely, to unpacking and setting them up at your new location, we will handle every aspect of your move with sweetness and speed. You can trust us with your stuff, because we treat it like our own. We also offer storage solutions for those who need some extra space or time before or after their move.
+                    </p>
+                    <p>
+                        We have secure and climate-controlled facilities where you can store your items for as long as you need. We also offer packaging materials and services for those who want to pack their own things or need some help with it. We have boxes, bubble wrap, tape, labels, and everything else you might need to pack your items properly. We can also provide professional packing assistance if you prefer to leave it to the experts.
+                    </p>
+                    <p>
+                        No matter what your moving needs are, SweetMoves has you covered. We are the sweet spot for all your moving needs. Contact us today to get a free quote and book your move with us.
+                        You’ll love our sweet service and sweet prices. SweetMoves: The sweetest way to move your home or business.
+                    </p>
+
                 <h3>Our Values</h3>
                 <p>
                     At SweetMoves, we believe in the following values:
