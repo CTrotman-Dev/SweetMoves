8dcad8347fb2ed9ea7cf4bf4a1fafafe7a7270cd
CTrotman-Dev <c.trotman92@gmail.com>
2023-08-07 20:55:12 +0100

    update homepage

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,8 +2,35 @@ import React from "react";
 
 const HeroImage = (props) => {
     return (
-        <div className="hero-container">
-            <img src={props.imgSrc} alt="hero-image"/>
+        <div className="row hero-container">
+            <div className="col-md-6">
+                <div className="hero-text-container">
+
+                    <h1>SweetMoves: Making your move a sweet success</h1>
+                    {/* <h1>SweetMoves: Moving made sweet and easy, with a smile.</h1> */}
+                    <p>
+                        Welcome to SweetMoves, the Cardiff-based company that operates across the UK, offering home and office removals, storage, and packaging services.
+                    </p>
+                    <p>
+                        Whether you are moving across the street or across the country, we are here to make your move as sweet as possible. We have over 3 years of experience in the moving industry, and we pride ourselves on our professionalism, reliability, and affordability.
+                    </p>
+                    <p>
+                        We have a fleet of modern vehicles, a team of trained and friendly staff, and a range of flexible options to suit your needs and budget. We also provide free quotes, insurance cover, and customer support throughout your move. At SweetMoves, we understand that moving can be stressful and time-consuming, but we are here to take care of everything for you.
+                    </p>
+                    <p>
+                        From packing your belongings with care and efficiency, to transporting them safely and securely, to unpacking and setting them up at your new location, we will handle every aspect of your move with sweetness and speed. You can trust us with your stuff, because we treat it like our own. We also offer storage solutions for those who need some extra space or time before or after their move.
+                    </p>
+                    <p>
+                        We have secure and climate-controlled facilities where you can store your items for as long as you need. We also offer packaging materials and services for those who want to pack their own things or need some help with it. We have boxes, bubble wrap, tape, labels, and everything else you might need to pack your items properly. We can also provide professional packing assistance if you prefer to leave it to the experts.
+                    </p>
+                    <p>
+                        No matter what your moving needs are, SweetMoves has you covered. We are the sweet spot for all your moving needs. Contact us today to get a free quote and book your move with us.
+                        You’ll love our sweet service and sweet prices. SweetMoves: The sweetest way to move your home or business.
+                    </p>
+                </div>
+            </div>
+            <div className="col-md-6"><img src={props.imgSrc} alt="hero-image" className="hero-img" /></div>
+
 
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,34 @@ import HeroTabbed from "../components/HeroTabbed";
 const Home = () => {
     return (
         <div>
-        <CarouselComponent />
-        <HeroTabbed />
+            <HeroImage imgSrc="images/img4-lg2.jpg" />
+            <div>
+                <div className="why-us-container">
+                    <div className="row">
+                        <div className="col-md-3 why-us-section">
+                            <h1>Quality equipment</h1> We have a fleet of modern and well-maintained vehicles that can accommodate any size or type of move. We also have high-quality moving equipment and tools, such as dollies, straps, blankets, pads, and ramps, to ensure that your items are loaded and unloaded safely and securely.
+                        </div>
+                        <div className="col-md-3 why-us-section">
+                            <h1>Competitive prices</h1> We offer fair and affordable prices for our services, without compromising on quality or safety. We also have no hidden fees or extra charges, so you can be sure that the price we quote is the price you pay. We also have various discounts and offers for our loyal and new customers, such as 10% off for referrals, 15% off for repeat customers, and 20% off for students and seniors.
+                        </div>
+                        <div className="col-md-3 why-us-section">
+                            <h1>Professional staff</h1> We have a team of trained and experienced movers who are friendly, courteous, and respectful. They will handle your belongings with care and efficiency, and make sure that everything is delivered to your new location in the same condition as it was when it left your old one. They will also follow your instructions and preferences, and answer any questions or concerns you might have along the way.
+                        </div>
+
+                        <div className="col-md-3 why-us-section">
+                            <h1>Customer satisfaction</h1> We are committed to providing you with the best moving experience possible. We have a 100% satisfaction guarantee policy, which means that if you are not happy with our service for any reason, we will do everything we can to make it right. We value your feedback and suggestions, and we strive to exceed your expectations.
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <HeroTabbed />
+            <CarouselComponent />
+
             {/* <HeroImage imgSrc="/logo.png" /> */}
-            <AnimateInOut visible="true" delay="0" in="animate__fadeInUp" out="">
+            {/* <AnimateInOut visible="true" delay="0" in="animate__fadeInUp" out="">
                 <QuickLinks />
-            </AnimateInOut>
+            </AnimateInOut> */}
         </div>
     );
 }
